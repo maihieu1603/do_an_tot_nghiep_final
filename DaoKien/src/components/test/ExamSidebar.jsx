1bda6322b7897eac2aed6523aff0f6c9e6f92fbc
Mai Xuan Hieu <maixuanhieuytt@gmail.com>
2025-12-25 23:54:10 +0700

    Revert "Merge branch 'main' of https://github.com/maihieu1603/do_an_tot_nghiep_final"
    This reverts commit 95426d15e5738cea7e70b0c39876e6247677fbf8, reversing
    changes made to a295e54a775ee79d95d9fcbc30daac2460ec002a.

--- DaoKien/src/components/test/ExamSidebar.jsx
+++
@@ -1,7 +0,0 @@
-export default function ExamSidebar() {
-    return (
-        <div className="w-80 hidden lg:block">
-            <p className="text-gray-500 text-sm">Chưa có dữ liệu user từ backend.</p>
-        </div>
-    )
-}
